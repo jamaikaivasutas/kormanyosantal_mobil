d8bc6ed71cb33fc35b382d3fb47da08cf20781fb
jamaikaivasutas <b1duzale@vasvari.org>
2025-09-10 10:31:55 +0200

    kesz a feladat

--- 02_feladat/adattipusfeladat.dart
+++ 02_feladat/adattipusfeladat.dart
@@ -6,4 +6,17 @@ void main() {
 
   int intResult = intValue * 5;
   double doubleResult = doubleValue / 0.54;
+
+  //intResult érték kiíratása
+  print("Az integer értéke $intResult");
+  
+  //doubleResult érték kiíratása
+  print("A double értéke $doubleResult");
+
+  
+
+  bool boolResult = !boolValue;
+
+  //boolResult értéke (boolValue negáltja)
+  print("A boolResult értéke $boolResult");
 }
